d16ef351f59e7625330656effb682d3add3c383d
Denise Perry <dbperry2@asu.edu>
2017-08-28 15:49:05 -0700

    Unwrapping fucking works

--- dbperry2_lab1/ViewController.swift
+++ dbperry2_lab1/ViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by dbperry2 on 8/28/17.
 //  Copyright © 2017 dbperry2. All rights reserved.
+//  https://github.com/dbperry17/MacLab1.git
 //
 
 import UIKit
@@ -26,10 +27,20 @@ class ViewController: UIViewController {
     
     @IBAction func bmiButton(_ sender: UIButton) {
         
-        let hText = Int(self.hBox.text!)!
-        
-        print(hText)
-        
+        if Int(self.hBox.text!) != nil && Int(self.wBox.text!) != nil
+        {
+            let hVal = Int(self.hBox.text!)!
+            let wVal = Int(self.wBox.text!)!
+            
+            print("Height = \(hVal)")
+            print("Weight = \(wVal)")
+        }
+        else
+        {
+            print("Missing height and/or weight")
+        }
+    
     }
+
 }
 
